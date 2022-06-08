341650bf085f2a7084cff810cb3fe0ffd968292a
DaviMoreira27 <davimsantana2706@gmail.com>
2022-06-08 20:00:01 -0300

    Finalização do projeto com o pokeAPI

--- Pokedex API/app.js
+++ Pokedex API/app.js
@@ -1,17 +1,11 @@
 const numbPokemons = document.querySelector("#pokNum");
-const tipoPokemon = document.getElementById("tipos");
-
-
-tipoPokemon.addEventListener('change', () => {
-  console.log(tipoPokemon.value);
-});
-
-
-
+const tipoPokemon = document.querySelector("#tipos");
 let numeroChange;
-let typePoke;
 
-const numbFilter = addEventListener("input", (event) => {
+// TODO: Pelo amor de Deus, otimiza esse codigo o mais rapido possivel. - 08/06/2022
+
+  const numbFilter = addEventListener("input", (event) => {
+  tipoPokemon.addEventListener("change", () => {
   let numeroChange = event.target.value.trim();
 
   const fetchAllPokemon = () => {
@@ -27,30 +21,46 @@ const numbFilter = addEventListener("input", (event) => {
     Promise.all(pokemonPromises).then((pokemons) => {
       const listPokemon = pokemons.reduce((accumulator, pokemon) => {
         const types = pokemon.types.map((typeInfo) => typeInfo.type.name);
-            
-        if (types[0] == "steel") {
-          accumulator += `
+        
+          if (types[0] == tipoPokemon.value) {
+            accumulator += `
             <li class="card ${types[0]}"> 
                 <img class="card-image" alt="${
                   pokemon.name
                 }" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-            pokemon.id
-          }.png">
+              pokemon.id
+            }.png">
                 <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
                 <p class="card-subtitle">${types.join(" | ")}</p>
             </li>
             `;
-        }
-        return accumulator;
+            
+        } if (tipoPokemon.value == 'nenhum') {
+            accumulator += `
+            <li class="card ${types[0]}"> 
+                <img class="card-image" alt="${
+                  pokemon.name
+                }" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+              pokemon.id
+            }.png">
+                <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+                <p class="card-subtitle">${types.join(" | ")}</p>
+            </li>
+            `;
+          }
+          return accumulator;
       }, "");
 
       const ul = document.querySelector('[data-js="pokedex"');
 
       ul.innerHTML = listPokemon;
+      console.log(listPokemon);
+      
     });
   };
 
   fetchAllPokemon();
 });
-
+    console.clear();
+});
 numbFilter;
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Portfolio
 The main projects that I made so far. I'm studying design, back-end and front-end, here I get all that I learned until now and gather in this repository.
+
+
+
+
+
+
+
+
+
