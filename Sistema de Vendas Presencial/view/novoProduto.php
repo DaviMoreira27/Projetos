3a9c45c5b3bfc0ad2d74514dcb66afda65cc22bb
DaviMoreira27 <davimsantana2706@gmail.com>
2022-07-30 11:54:51 -0300

    Inicio do projeto To Do List em typescript, este projeto tem viés de estudo, portanto pode apresentar eventuais erros

--- Sistema de Vendas Presencial/view/novoProduto.php
+++ Sistema de Vendas Presencial/view/novoProduto.php
@@ -8,5 +8,13 @@ require_once __DIR__ . '/navbar.php';
             <input type="file" name="prodPicture" id="imagem-produto">
         </label>
 
+        <div class="titles-prod-introdcution">
+            <label for="titulo-prod">
+                <input type="text" placeholder="Nome do Produto" name="titulo-prod" id="titulo-prod">
+            </label>
+            <label for="titulo-prod">
+                <input type="text" placeholder="Nome do Produto" name="titulo-prod" id="titulo-prod">
+            </label>
+        </div>
     </div>
 </form>
